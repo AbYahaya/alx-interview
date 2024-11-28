189247f8459adaae73d3c9c8f3a223206fa85c46
AbYahaya <yabdulrauf911@gmail.com>
2024-11-28 23:51:13 +0000

    Some file

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A file working with DSA
+"""
+
+
+def makeChange(coins, total):
+    """Returns the fewest number of coins needed to meet a given total."""
+    if total <= 0:
+        return 0
+    
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
+    
+    for coin in coins:
+        for amount in range(coin, total + 1):
+            dp[amount] = min(dp[amount], dp[amount - coin] + 1)
+    
+    return dp[total] if dp[total] != float('inf') else -1
